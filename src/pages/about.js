2730cc8c49340f3f12a4d8aa43a60dd86573c4dd
Hanif Setyananda <hanifsetya@dinustek.com>
2022-06-07 15:36:23 +0700

    initial commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="About Page">
     <p>I'm making this by following the Gatsby Tutorial.</p>
-    <StaticImage 
-      alt='Bla bla'
-      src='../images/example.png'
-    />
   </Layout>
   )
 }
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
